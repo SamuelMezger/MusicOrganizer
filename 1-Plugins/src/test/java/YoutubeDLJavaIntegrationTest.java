63ba05955b7678754e9357d902f921e8ce2b716d
Samuel Mezger <samuel.mezger@kit.edu>
2020-11-05 14:32:12 +0100

    added youtubedl-java dependency to pom.xml added integration test to test abilities of installed youtube-dl

---
+++ 1-Plugins/src/test/java/YoutubeDLJavaIntegrationTest.java
@@ -0,0 +1,37 @@
+import com.sapher.youtubedl.*;
+import com.sapher.youtubedl.mapper.VideoInfo;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class YoutubeDLJavaIntegrationTest {
+
+    private final static String MusicVid_TheFatRat_RuleTheWorld_ID = "OJdG8wsU8cw";
+    private final static String YTMusic_TheFatRat_RuleTheWorld_ID = "es__dhV4nT8";
+
+    @Before
+    public void setUp() {
+        YoutubeDL.setExecutablePath("youtube-dlc");
+    }
+
+    @Test
+    public void MusicInVid_extractable() throws YoutubeDLException {
+        VideoInfo videoInfo = YoutubeDL.getVideoInfo(MusicVid_TheFatRat_RuleTheWorld_ID);
+        Assert.assertEquals(
+                "YoutubeDL should be able to extract metadata from the 'Music in this Video Section'",
+                "Rule The World, TheFatRat, Rule The World",
+                String.format("%s, %s, %s", videoInfo.track, videoInfo.artist, videoInfo.album)
+        );
+    }
+    
+    @Test
+    public void YTMusic_description_extractable() throws YoutubeDLException {
+        VideoInfo videoInfo = YoutubeDL.getVideoInfo(YTMusic_TheFatRat_RuleTheWorld_ID);
+        Assert.assertEquals(
+                "YoutubeDL should be able to extract metadata from the description of YTMusic tracks",
+                "Rule the World, TheFatRat, AleXa, Rule the World",
+                String.format("%s, %s, %s", videoInfo.track, videoInfo.artist, videoInfo.album)
+        );
+    }
+}
